586c87da998ae54b7e55c4dc1f2be4aaf548a2c2
gigazelle <33764851+gigazelle@users.noreply.github.com>
2023-02-01 02:13:41 +0000

    Deploying to gh-pages from  @ d5105aa582ac3276db5bd3d82bf159f4ccbe25b7 🚀

---
+++ component---src-pages-api-md-c70c68fcbd1b308be5be.js
@@ -0,0 +1,2 @@
+(self.webpackChunkdev_site_documentation_template=self.webpackChunkdev_site_documentation_template||[]).push([[1980],{3775:function(t,n,e){"use strict";e.r(n),e.d(n,{_frontmatter:function(){return m},default:function(){return p}});var o=e(2122),u=e(9756),r=(e(5007),e(4983)),a=e(9536),c=["components"],m={},s={_frontmatter:m},i=a.Z;function p(t){var n=t.components,e=(0,u.Z)(t,c);return(0,r.mdx)(i,(0,o.Z)({},s,e,{components:n,mdxType:"MDXLayout"}))}p.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-api-md-c70c68fcbd1b308be5be.js.map
