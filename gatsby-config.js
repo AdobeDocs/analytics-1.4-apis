30b44b0f275b7263237b69e99da8e7edc225cfa4
John Wight <johnwight@gmail.com>
2023-04-20 17:06:23 -0600

    Update gatsby-config.js
    Removing Livestream from 1.4 TOC

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,28 +114,6 @@ module.exports = {
         path: 'guides/data-sources.md'
       },
       {
-        title: 'Livestream API',
-        path: 'guides/livestream/index.md',
-        pages: [
-          {
-            title: 'Variable reference',
-            path: 'guides/livestream/variable-reference.md'
-          },
-          {
-            title: 'Example output',
-            path: 'guides/livestream/example-output.md'
-          },
-          {
-            title: 'Troubleshooting',
-            path: 'guides/livestream/troubleshooting.md'
-          },
-          {
-            title: 'FAQ',
-            path: 'guides/livestream/faq.md'
-          }
-        ]
-      },
-      {
         title: 'Reporting API',
         path: 'guides/reporting/index.md',
         pages: [
